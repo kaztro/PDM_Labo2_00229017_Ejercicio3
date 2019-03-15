f73f6b727a0b31fce06a81ad2399c7238b5f9552
karichalobato <00002517@uca.edu.sv>
2019-03-15 15:03:17 -0600

    Commit inicial we

---
+++ app/src/main/java/com/example/pdm_labo2_00229017_e3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pdm_labo2_00229017_e3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
