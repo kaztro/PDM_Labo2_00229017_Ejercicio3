f4408b7fb845f491aa142c71a04a566439d014f6
karichalobato <00002517@uca.edu.sv>
2019-03-15 15:31:25 -0600

    XML is complete...

--- app/src/main/java/com/example/pdm_labo2_00229017_e3/MainActivity.java
+++ app/src/main/java/com/example/pdm_labo2_00229017_e3/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.pdm_labo2_00229017_e3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
